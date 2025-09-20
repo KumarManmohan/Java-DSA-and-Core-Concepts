c557493cd399f86b3ba247dc50f3fc269279662a
Your Name <you@example.com>
2025-09-20 14:09:08 +0530

    Add HelloWorld.java example

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is a new file.");
+        System.out.println("Now I will push this to GitHub.");
+    }
+}
